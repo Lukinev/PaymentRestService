cbe955912c22f015606eba90b94e28a6cc994135
Lukinev <lukinev@ukr.net>
2018-11-12 12:49:30 +0200

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var conf = require('../config');
-
-router.all('/', require('./' + conf.version + '/index'));
-router.all('/account/*', require('./' + conf.version + '/account'));
-router.all('/payment/*', require('./' + conf.version + '/payment'));
-
-module.exports = router;
