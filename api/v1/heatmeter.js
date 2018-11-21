ddd14770761020dff482c57a65aac223d1b9e450
s.vladimirov <vladimirovsk@gmail.com>
2018-11-21 23:03:01 +0200

    hetmeter get

--- api/v1/heatmeter.js
+++ api/v1/heatmeter.js
@@ -6,23 +6,44 @@ const
     { checkJWT } = require('./libs/auth'),
     libs = require('./libs/functions');
 router = new Router(),     
-/*
-     * sn - серийный номер
-     * power
-     * flow
-     * temp1
-     * temp2
-     * energy1
-     * id_heatmeter 
-     */
 
+
+router.get('/heatmeter/setParams', async(req, res)=>{
+    const h = await libs.execQuery(models.heatmeterGetId, [req.query.sn], global.pool_heatmeter);
+    const id_heatmeter = h.rows[0].id;
+    if (id_heatmeter>0){
+        console.log(h.rows[0].id);
+        const r = await libs.execQuery(models.heatmeterSetPararams, [0, 
+                                                                    id_heatmeter, 
+                                                                    req.query.power, 
+                                                                    req.query.flow, 
+                                                                    req.query.energy,
+                                                                    req.query.temp1, 
+                                                                    req.query.temp2, 
+                                                                    moment().format('DD.MM.YYYY'),
+                                                                    moment().format('hh:mm:ss')
+                                                                ], 
+                                                                global.pool_heatmeter);
+                                                                
+        const u = await libs.execQuery(models.hetmeterUpdatePower, [ id_heatmeter,
+                                                                    parseInt(req.query.energy,10),
+                                                                    moment().format('DD.MM.YYYY HH:mm:ss')
+                                                                    ],
+                                                                    global.pool_heatmeter);                                                        
+                                                                
+        res.status(200).json({ "status": 200, "error": null, "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": r.rows });
+    
+    }else
+        res.status(400).json({ "status": 400, "error": "Not find heatmeter: "+req.query.sn, "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": null });
+    //console.log(req.query.sn);
+    //res.send(req.query.id);
+}), 
 
 router.post('/heatmeter/setParams', async (req, res) => {
      if ((await checkJWT(req.body)).status === 200) {
         const h = await libs.execQuery(models.heatmeterGetId, [req.body.sn], global.pool_heatmeter);
         const id_heatmeter = h.rows[0].id;
         if (id_heatmeter>0){
-            console.log(h.rows[0].id);
             const r = await libs.execQuery(models.heatmeterSetPararams, [req.body.client_id, 
                                                                         id_heatmeter, 
                                                                         req.body.power, 
