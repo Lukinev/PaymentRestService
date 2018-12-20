6d69e6cf66cf288637575b3797dd04baa0b45c72
s.vladimirov <vladimirovsk@gmail.com>
2018-12-20 17:19:58 +0200

    payments

--- api/v1/models/models_payment.js
+++ api/v1/models/models_payment.js
@@ -10,13 +10,37 @@ module.exports = models = {
     
     paymentNewPackage: {
         name: 'create new payment package',
-        required_fields: ['terminal_id',
+        required_fields: 
+        
+        ['terminal_id',
             'account',
             'amount',
-            'service_id'],
+            'period_id',
+            'service_id',
+            'data_pay',
+            'provider_id',
+            'client_id',
+            'client_name',
+            'token'
+        ],
+
         text: `with pack as (
-            insert into public.packages(terminal_id) values($1) RETURNING id  
+            insert into public.packages(client_id) values($1) RETURNING id  
          )
-         insert into public.payments(package_id, service_id, amount) values((select id from pack), $2,$3) RETURNING *`
-    }
+         insert into public.payments(package_id, service_id, amount, provider_id) values((select id from pack), $2,$3,$4) RETURNING *`
+    },
+
+     paymentNewPay:{
+        name: 'create new payment',
+        required_fields: [
+            "uid",
+            "amount",
+            "service_id",
+            "provider_id",
+            "client_id",
+            "createpay",
+        ],
+        
+        text:`insert into public.payments(uid, amount, service_id, provider_id, createpay, client_id) values ($1,$2,$3,$4,$5,$6) returning id, amount, created_at`
+     }
 }
--- api/v1/payment.js
+++ api/v1/payment.js
@@ -13,11 +13,21 @@ router.get('/payment/', (req, res) => {
 router.post('/payment/create', async (req, res) => {
     
     if ((await checkJWT(req.body)).status === 200) {
-            if (req.body.terminal_id && typeof (req.body.terminal_id) === 'number') {
-                const r = await libs.execQuery(models.paymentNewPackage, [req.body.terminal_id, req.body.service_id, req.body.amount], global.pool_payment);
-                res.status(200).json({ "status": 200, "error": null, "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": r.rows });
-            } else
-                res.status(400).json({ "status": 400, "error": "Bad request", "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": null });
+           // if (req.body.package_id ===&& typeof (req.body.client_id) === 'number') {
+//                const r = await libs.execQuery(models.paymentNewPackage, [req.body.terminal_id, req.body.service_id, req.body.amount], global.pool_payment);
+
+                const r = await libs.execQuery(models.paymentNewPay,[
+                    req.body.uid, 
+                    req.body.amount, 
+                    req.body.service_id, 
+                    req.body.provider_id, 
+                    req.body.createpay, 
+                    req.body.client_id
+                ], global.pool_payment);
+
+                res.status(200).json({ "status": 200, "error": null, "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": r.rows});
+           // } else
+             //   res.status(400).json({ "status": 400, "error": "Bad request", "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": null });
     } else
         res.status(400).json({ "status": 400, "error": "Bad autorized token", "timestamp": moment().format('DD.MM.YYYY hh:mm:ss.SSS'), "dataset": null });
 });
